546eeb60b2ffe748f40291695d5c80f2800cce2e
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2023-01-28 15:32:12 +0100

    add Anton Lesson

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -1,9 +1,30 @@
+import { IconContext } from 'react-icons';
+
 import {
   StatisticBox,
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+import PropTypes from 'prop-types';
+
+export const StatisticItem = ({ elem: { title, total }, icon }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ size: 40, color: 'green' }}>
+        {icon}
+      </IconContext.Provider>
+
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
+};
+
+StatisticItem.propTypes = {
+  elem: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired,
+  }).isRequired,
+  icon: PropTypes.object,
 };
